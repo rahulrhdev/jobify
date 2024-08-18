980882ebd0de4856d4dea923c8166bc875c50c6c
rahulrhdev <rahulrhdev@gmail.com>
2024-08-18 11:17:11 +0530

    utils init

---
+++ utils/links.tsx
@@ -0,0 +1,25 @@
+import { AreaChart, Layers, AppWindow } from 'lucide-react';
+
+type Navlink = {
+    href: string;
+    label: string;
+    icon: React.ReactNode;
+}
+
+const links: Navlink[] = [
+    {
+        href: '/add-job',
+        label: 'add job',
+        icon: <Layers />,
+    },
+    {
+        href: '/jobs',
+        label: 'jobs',
+        icon: <AppWindow />
+    },
+    {
+        href: '/stats',
+        label: 'stats',
+        icon: <AreaChart />
+    }
+]
